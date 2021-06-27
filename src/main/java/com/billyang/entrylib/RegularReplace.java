ad2551994ea059e0ebff4e87e072d8af02d77ce3
Bill_Yang <ym13883407586@outlook.com>
2021-06-28 05:43:20 +0800

    replacer set

---
+++ src/main/java/com/billyang/entrylib/RegularReplace.java
@@ -0,0 +1,34 @@
+package com.billyang.entrylib;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegularReplace {
+
+    String pattern,line,answer;
+    int id;
+
+    RegularReplace(int id,String pattern,String line,String answer) {
+        this.id = id;
+        this.pattern = pattern;
+        this.line = line;
+        this.answer = answer;
+    }
+
+    String replace(StringBuilder ErrorInfo) {
+
+        Pattern pt = Pattern.compile(pattern);
+        Matcher mt = pt.matcher(line);
+
+        if(mt.find()) {
+            int size = mt.groupCount();
+            for(int i = 1; i <= size; i++)answer = answer.replace("$" + i, mt.group(i));
+            return answer;
+        } else {
+            ErrorInfo.append("正则替换时失配！\n");
+            return null;
+        }
+
+    }
+
+}
